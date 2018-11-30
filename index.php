338ae5ece6ac274539d66b1d946f7e8409525bc1
Tony Mourier <mourier.tony@outlook.com>
2018-11-30 14:40:26 +0100

    Mise en place de la barre de mercure

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 $json_source = file_get_contents('data.txt');
 $json_data = json_decode($json_source);
+$degres = $json_data->temperature;
+$hauteurcalc = (161 + ($degres * 4));
+$topcalc = (315 + -($degres * 4));
+$edittime = date("d/m/Y H:i:s.",filemtime("data.txt"));
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,7 +15,17 @@ $json_data = json_decode($json_source);
 </head>
 <body>
 <h1>Température</h1>
+<p>Date de prise de température : <?php echo $edittime ?> </p>
 <p>Il fait <?php echo $json_data->temperature ; ?>°C avec <?php echo $json_data->humidite ; ?>% d'humidité.</br></p>
-<div id="thermometer"> <div id="bargraph"></div> </div>
+<div id="thermometer"> 
+	<div id="bargraph"></div> 
+</div>
 </body>
 </html>
+
+<style type="text/css">
+	#bargraph {
+		top: <?php echo $topcalc."px" ?>;
+		height: <?php echo $hauteurcalc."px" ?>;
+	}
+</style>
