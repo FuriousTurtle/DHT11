160a170e39dcbab37ca8893e1a644a1269766dc4
Tony Mourier <mourier.tony@outlook.com>
2018-11-30 10:14:21 +0100

    Initialisation

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 echo('Hello world!');
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<script type="text/javascript" src="jquery.js"></script>
+	<meta charset="utf-8">
+	<title>Thermomètre</title>
+</head>
+<body>
+
+	<div id="main"><img src="thermometer.jpg"></div>
+
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
