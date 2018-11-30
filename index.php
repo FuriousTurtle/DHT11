f8075c50d88cda82e914e82abcceed64d1688937
Tony Mourier <mourier.tony@outlook.com>
2018-11-30 10:20:54 +0100

    Modifs

--- index.php
+++ index.php
@@ -4,14 +4,18 @@ echo('Hello world!');
 <!DOCTYPE html>
 <html>
 <head>
-	<script type="text/javascript" src="jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+	<script type="text/javascript" src="scripts/jquery.js"></script>
 	<meta charset="utf-8">
 	<title>Thermomètre</title>
 </head>
 <body>
 
-	<div id="main"><img src="thermometer.jpg"></div>
+	<div id="main">
+		<h1>Température</h1>
+		<img src="img/thermometer.jpg">
+	</div>
 
-<script src="script.js"></script>
+<script src="scripts/script.js"></script>
 </body>
 </html>
